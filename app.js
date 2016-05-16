949a884a99a179711bb62584d961ff2e1ac3ef13
Coleman <rollins@50000feet.com>
2016-05-16 16:03:40 -0500

    Awww yisss got mongo working with the email submit form :D

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var users = require('./routes/users');
 
 var app = express();
 
+// db
+var MongoClient = require('mongodb').MongoClient;
+var db;
+
+MongoClient.connect('mongodb://trunk:trunkRoot@ds019482.mlab.com:19482/trunk-production', (err, database) => {
+  if (err) return console.log(err)
+  db = database;
+})
+
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
@@ -18,12 +27,22 @@ var app = express();
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// Form handling
+app.post('/emails', (req, res) => {
+  db.collection('emails').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/');
+  })
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
